3e5cc02f9d86d24ca2cfd613d2ac2702177d1b53
polytropoi <polytropoi@gmail.com>
2016-02-23 17:25:33 -0600

    init

---
+++ Assets/GuiMangler.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GuiMangler : MonoBehaviour {
+
+	public NetMangler netMangler;
+	public Text serverStatus;
+	public Text hosts;
+	public GameObject initButton;
+	public GameObject serverButton;
+	public GameObject listenButton;
+	public GameObject hostButton;
+	public GameObject joinButton;
+	public string hostIP;
+	public string hostsString = "";
+
+	public Text joinButtonLabel;
+	// Use this for initialization
+	void Start () {
+		serverButton.SetActive (false);
+		listenButton.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InitializeButton() {
+		netMangler.Init ();
+		initButton.SetActive (false);
+		serverButton.SetActive (true);
+		listenButton.SetActive (true);
+		serverStatus.text = netMangler.discovery.broadcastData;
+	}
+
+	public void ServerButton() {
+		netMangler.StartDiscoveryServer ();
+		serverButton.SetActive (false);
+		listenButton.SetActive (false);
+		hostButton.SetActive (true);
+//		joinButton.SetActive (true);
+		serverStatus.text = "Broadcasting from " + netMangler.networkAddress;
+	}
+
+	public void ListenButton () {
+		netMangler.StartDiscoveryClient ();
+		serverButton.SetActive (false);
+		listenButton.SetActive (false);
+		joinButton.SetActive (true);
+		serverStatus.text = "Broadcast Listener Started";
+	}
+	public void IncomingHosts(string hostData) {
+//		if (!netMangler.IsClientConnected()) {
+//			hosts.text = hostData; 
+//			ShowJoinButton ();
+//		} else {
+//			HideJoinButton ();
+//			netMangler.discovery.StopBroadcast ();
+//		}
+		hosts.text = "host found at " + hostData; 
+		joinButtonLabel.text = hostData;
+	}
+
+	public void LanHostButton() {
+		netMangler.StartGameHost ();
+	
+	}
+	public void HideHostButton() {
+		hostButton.SetActive (false);
+	}
+	public void ShowJoinButton() {
+		joinButton.SetActive (true);
+	}
+	public void HideJoinButton() {
+		joinButton.SetActive (false);
+	}
+
+	public void LanClientButton() {
+		netMangler.StartGameClient ();
+	}
+}
---
+++ Assets/NetMangler.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+using UnityEngine.Networking;
+
+public class NetMangler : NetworkManager
+{
+	public GuiMangler guiMangler;
+	public NetworkDiscovery discovery;
+	public string serverdata;
+	NetworkClient myClient;
+	public string hostIP;
+
+	public Dictionary<string,UnityEngine.Networking.NetworkBroadcastResult> broadcasts;
+
+//	public override void OnStartHost()
+//	{
+//		discovery.Initialize();
+//		discovery.StartAsServer();
+//
+//	}
+
+
+	public void StartDiscoveryServer() {
+		discovery.StartAsServer ();
+	}
+
+	public void StartDiscoveryClient() {
+		discovery.StartAsClient ();
+	}
+
+	public void StartGameHost() {
+		StartHost ();
+	}
+	public void StartGameClient() {
+			
+		networkAddress = hostIP;
+		StartClient ();
+
+//			myClient = new NetworkClient();
+//			myClient.RegisterHandler(MsgType.Connect, OnConnected);     
+//			myClient.Connect(hostIP, 7777);
+	}
+//	public void OnConnected(NetworkMessage netMsg)
+//	{
+//		Debug.Log("Connected to server");
+//		guiMangler.IncomingHosts ("joined game at " + hostIP);
+//
+//	}
+	public void Init() {
+		discovery.Initialize ();
+		serverdata = discovery.broadcastData;
+		InvokeRepeating ("BroadcastCheck", 1f, 1f);
+	}
+	public override void OnStartHost() {
+		guiMangler.HideHostButton ();
+		guiMangler.HideJoinButton ();
+	}
+	public override void OnStartClient(NetworkClient client)
+	{
+		discovery.showGUI = false;
+		guiMangler.HideJoinButton ();
+		guiMangler.hosts.text = "joined host at " + networkAddress;
+		CancelInvoke ();
+	}
+
+
+	public override void OnStopClient()
+	{
+		discovery.StopBroadcast();
+		discovery.showGUI = true;
+	}
+
+	public void BroadcastCheck () {
+		
+		broadcasts = discovery.broadcastsReceived;
+		if (broadcasts != null) {
+			foreach (var pair in broadcasts) {
+				var key = pair.Key;
+				var value = pair.Value;
+
+				hostIP = key; //TODO chooser
+//				guiMangler.HideHostButton();
+//				guiMangler.ShowJoinButton ();
+			}
+		}
+	}
+//	public Update () {
+//		discovery.broadcastsReceived
+//	{
+//		guiMangler.IncomingHosts (data + " : " + fromAddress);
+//	}
+//	}
+}
